ada219da6ce05817c2dd28b6292a210edd4233bc
yemuc <yemuc98@gmail.com>
2019-05-23 21:08:11 +0800

    重新建立项目

--- src/test/ajax_test.php
+++
@@ -1,6 +0,0 @@
-<?php
-// echo '这是ajax';
-header('Content-Type:application/json; charset=utf-8');
-$arr = array('a'=>1,'b'=>2,'c'=>'哪');
-echo json_encode($arr,JSON_UNESCAPED_UNICODE);
-?>
--- src/test/post_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Access-Control-Allow-Origin:*');//注意！跨域要加这个头
-$student = $_POST['student'];
-echo json_encode($student,JSON_UNESCAPED_UNICODE);
-echo $student[0]['name'];
-// echo $student['age'];
-// echo $student['sex'];
-echo($_SERVER['HTTP_HOST']);
-?>
